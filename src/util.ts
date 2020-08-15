8da11cc1501935ea57dac63ccd925db462fb0485
Theo Paris <theoplaysmc@gmail.com>
2020-08-14 18:16:46 -0700

    d

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,24 @@
 export const hewwwo = (texty_wexty: string) => {
-    return texty_wexty
-        .replace(/(\w)(\w*)lge/g, "$1$2lgie w$2lgie")
-        .replace(/(\w)(\w*)uck/g, "$1$2lgie w$2lgie")
-        .replace(/[r|l]/g, "w")
-        .replace(/you/g, "uwu");
+	return texty_wexty
+		.replace(/(\w)(\w*)lge/g, "$1$2lgie w$2lgie")
+		.replace(/(\w)(\w*)uck/g, "$1$2lgie w$2lgie")
+		.replace(/[r|l]/g, "w")
+		.replace(/you/g, "uwu");
 };
 
+export const asyncify = (msg: string) =>
+    `
+    module.exports = async () => {
+        ${msg}
+    };
+    `;
+
+export const mainPath = () => __dirname + "/" + require.main?.filename;
+
 export type Config = {
-    token: string;
-    channels?: string[];
-    prefix?: string;
+	token: string;
+	channels?: string[];
+	prefix?: string;
+	botOwner?: string;
+	debug?: boolean;
 };
